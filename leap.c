5f65ea2c38d0d6147911034bbca953cccd63d61e
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-02-05 16:00:09 +0530

    leap.c

---
+++ leap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the year");
+scanf("%d",&a);
+if(a%4==0)
+{
+printf("ITS A LEAP YEAR");
+}
+else
+{
+printf("SORRY......ITS NOT A LEAP YEAR");
+}
+return 0;
+}
+
