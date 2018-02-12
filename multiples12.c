a27740c026afaaef7259e67cb5043d9e810f69f7
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-02-12 16:38:04 +0530

    multiples12.c

--- multiples12.c
+++ multiples12.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,i;
+int a,i,b;
 printf("enter the number");
 scanf("%d",&a);
 for(i=1;i<=5;i++)
 {
-a=a*i;
+b=a*i;
 printf("%d",a);
 printf("\t");
-a=5;
 }
 return 0;
 }
