2fe8bfab12ff54f1c058eaa0aa498cc66b8e079c
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-02-21 16:18:59 +0530

    sorting21.c

--- sorting21.c
+++ sorting21.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a[10],i,n,t,j;
+int a[10],i,n,b,j;
 printf("enter the range\n");
 scanf("%d",&n);
 printf("enter the array values\n");
@@ -15,9 +15,9 @@ for(j=0;j<n-i-1;j++)
 {
 if(a[i]<a[j])
 {
-t=a[i];
+b=a[i];
 a[i]=a[j];
-a[j]=t;
+a[j]=b;
 }
 }
 }
