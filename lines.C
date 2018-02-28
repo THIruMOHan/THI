d3d2199e26ae66bd04ac7eb1e7485a8b02f90f35
THIruMOHan <35688827+THIruMOHan@users.noreply.github.com>
2018-02-28 02:22:28 -0800

    lines.C

---
+++ lines.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	char str[50];
+	int a,count=1;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(a=0;str[a]!='\0';a++)
+	{
+		if(str[a]=='.')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nno of lines is=%d",count);
+	return 0;
+}
